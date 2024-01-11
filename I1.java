531d2fc8b3967c752fd6dbd93a20b9f048602663
KVSnehaDeepika <snehadeepikakamisetty@gmail.com>
2024-01-11 11:35:56 +0530

    added I1 interface

--- I1.java
+++ I1.java
@@ -3,3 +3,13 @@ interface I1
 public int Sum(int a,int b);
 public int Sub(int x,int y);
 }
+class Calci implements I1
+{
+public int Sum(int a,int b)
+{
+return a+b;
+}
+public int Sub(int x,int y)
+{
+return a-b;
+}
